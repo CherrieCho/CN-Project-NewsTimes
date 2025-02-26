44e5f72313901642d69bd9a552d63fdf3e9a038f
cherrie <cherry199848@gmail.com>
2025-02-26 12:26:21 +0900

    modified url

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let news = [];
 //뉴스 가져오기
 const getLatestNews = async () => {
   const url = new URL(
-    `https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?country=kr`
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
   );
   const response = await fetch(url);
   const data = await response.json();
