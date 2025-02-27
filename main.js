42c275c93da76401eb598e332f3bcbcd8079da22
cherrie <cherry199848@gmail.com>
2025-02-27 23:34:44 +0900

    code refactored

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const apiKey = ``;
 let newsList = [];
+let url = new URL(
+  `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
+);
 
 const headLine = document.querySelector(".head-line svg");
 const menus = document.querySelectorAll(".menu button");
@@ -8,40 +11,52 @@ menus.forEach((button) =>
   button.addEventListener("click", (event) => getNewsByCategory(event))
 );
 
-//뉴스 가져오기
-const getLatestNews = async () => {
-  const url = new URL(
-    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
-  );
+//뉴스 가져와서 배열 업뎃 후 렌더하는 함수(공통부분)
+const getNews = async () => {
   const response = await fetch(url);
   const data = await response.json();
   newsList = data.articles;
   render();
 };
 
+//전체 뉴스 가져오기
+const getLatestNews = async () => {
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
+  );
+  getNews();
+};
+getLatestNews();
+
 //카테고리별 뉴스
 const getNewsByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
-  const url = new URL(
+  url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`
   );
-  const response = await fetch(url);
-  const data = await response.json();
-  newsList = data.articles;
-  render();
+  getNews();
+};
+
+//키워드별로 검색
+const getNewsByKeyword = async (event) => {
+  event.preventDefault();
+  const keyword = document.getElementById("search-input").value.toLowerCase();
+  url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}`
+  );
+  getNews();
 };
 
 //모바일 사이드바 카테고리
 /* Set the width of the side navigation to 300px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
-  const sideMenus = document.querySelectorAll(".sidenav a");
-  //a 태그 중에서 첫번째만 빼고 이벤트리스너 추가
-  for (let i = 1; i < sideMenus.length; i++) {
-    sideMenus[i].addEventListener("click", function (event) {
-      getNewsByCategory(event);
-    });
-  }
+  const sideMenus = document.querySelectorAll(".sidenav button");
+
+  //button 태그에 이벤트리스너 추가
+  sideMenus.forEach((button) =>
+    button.addEventListener("click", (event) => getNewsByCategory(event))
+  );
 }
 
 /* Set the width of the side navigation to 0 */
@@ -55,6 +70,7 @@ const toggleSearch = () => {
   searchContainer.classList.toggle("active");
 };
 
+//newsList를 보여주는 render()
 const render = () => {
   const newsHTML = newsList.map((item) => {
     const time = moment(item.publishedAt).startOf("day").fromNow();
@@ -87,5 +103,3 @@ const render = () => {
 
   document.querySelector("#news-board").innerHTML = newsHTML.join("");
 };
-
-getLatestNews();
